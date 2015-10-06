66a6d153d5d823f0578769af20a5d115c0f51d97
Jack3113 <jack03113@gmail.com>
2015-10-06 09:10:39 +0200

    Remove multi browser in testing

--- Tests/conf.js
+++ Tests/conf.js
@@ -1,14 +1,5 @@
 exports.config = {
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
-  
-  multiCapabilities: [
-		{
-            'browserName': 'chrome'
-        },
-		{
-            'browserName': 'firefox'
-        }
-    ]
+  specs: ['spec.js']
 }
